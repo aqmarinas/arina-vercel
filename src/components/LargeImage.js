942a05dfe2e024cccfcf272def803a4eba38877c
Arina <aqmarinasha@gmail.com>
2024-09-15 16:34:22 +0700

    fix: format large image

--- src/components/LargeImage.js
+++ src/components/LargeImage.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const LargeImage = () => (
   <div className="relative w-full h-60 md:h-96 mb-4">
-    <Image src="/img/arina.jpg" alt="Arina" layout="fill" objectFit="cover" />
+    <Image src="/img/arina.webp" alt="Arina" layout="fill" objectFit="cover" />
   </div>
 );
 
