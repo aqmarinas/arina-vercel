b9934c48dcb33abf902175212376fce352c85f33
Arina <aqmarinasha@gmail.com>
2025-06-08 13:00:47 +0700

    remove unused imports

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,4 @@
-import { useMediaQuery } from "react-responsive";
 import Container from "../components/layout/Container.jsx";
-import { Minus, Square, X } from "lucide-react";
 import CTA from "../components/section/CTA.jsx";
 import Experiences from "../components/section/Experiences.jsx";
 import Projects from "../components/section/Projects.jsx";
@@ -10,8 +8,6 @@ import Footer from "../components/section/Footer.jsx";
 import AboutMe from "../components/section/AboutMe.jsx";
 
 export default function Home() {
-  const isMdOrSmaller = useMediaQuery({ query: "( 767px)" });
-
   return (
     <Container>
       <AboutMe />
